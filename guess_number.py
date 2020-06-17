878695921f26fc16a964dd4b413bbe514e1d99b1
Wu Lucas <lucas12121992@gmail.com>
2020-06-17 11:44:18 -0300

    Last version

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,20 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1  # count = count + 1
     num = input('Guess a number: ')
     num = int(num)
     if num == r:
         print('Bingo!!!')
+        print(count, 'times') #第一個方式 (老師)
         break
     elif num > r:
         print('Your answer is bigger.')
     elif num < r:
         print('Your answer is smaller')
+    print(count, 'times')
+#print(count, 'times') #第二個方式 (自己)
+
+   
